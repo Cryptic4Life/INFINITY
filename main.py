874db856822b1ad6dea31613c22cc9859660af4d
André Cerqueira <andrew57.cerqueira@gmail.com>
2024-10-26 11:35:35 +0100

    added constants

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ stock = {
     "poliéster": 2200
 }
 
+PROCURA_ANUAL = 50_000
+CUSTO_DE_ENCOMENDA = 10
+CUSTO_DE_POSSE = 0.70
+PRAZO_DE_ENTREGA = 7
+STOCK_DE_SEGURANÇA = 1000
+
 def remove_product(list_of_products, name):
     list_of_products.remove(name)
     return 0
